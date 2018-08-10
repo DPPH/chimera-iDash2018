b434865927e30b63056fdae16ac30913faaf05c4
Sergiu Carpov <ssmiler@gmail.com>
2018-08-10 19:07:16 +0200

    log_regr: verify if path is applicable before applying it

--- log_regr/patch_tfhe_torus_generic.bash
+++ log_regr/patch_tfhe_torus_generic.bash
@@ -1,5 +1,5 @@
 PATCH_DIR=$(cd `dirname $0` && pwd)
 echo $PATCH_DIR
 cd $PATCH_DIR/external/tfhe_torus_generic
-git am --signoff -k $PATCH_DIR/tfhe_torus_generic.patch
+git apply --check $PATCH_DIR/tfhe_torus_generic.patch && git am --signoff -k $PATCH_DIR/tfhe_torus_generic.patch
 cd -
