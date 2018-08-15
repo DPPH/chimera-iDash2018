43560dde4bc049df3f232ae37438e01b821f8013
Mariya <mariya@inpher.io>
2018-08-15 15:23:36 +0200

    debut unitest external product

--- fhe/test/trgsw_test.cpp
+++ fhe/test/trgsw_test.cpp
@@ -1,5 +1,33 @@
 #include <include/gtest/gtest.h>
+#include "../TRLwe.h"
+#include "../TRGSW.h"
+
+using namespace std;
 
 TEST(TRGSW_TEST, trgsw_external_product) {
     ASSERT_FALSE(2);
+
+
+    int64_t N = 64;
+    int64_t nblimbs_in = 5;
+
+
+    int64_t alpha_bits = nblimbs_in * BITS_PER_LIMBS - 2;
+    UINT64 out_alpha_bits = alpha_bits;
+
+
+    BigTorusParams bt_params_in(nblimbs_in);
+
+    TLweParams tlwe_params_in(N, bt_params_in);
+    shared_ptr<TLweKey> key = tlwe_keygen(tlwe_params_in);
+
+    TRGSWParams trgswParams(N, bt_params_in);
+    TRGSW a(trgswParams);
+
+
+    TRLweParams trlweParams(N, bt_params_in);
+    TRLwe b(trlweParams);
+
+
+
 }
