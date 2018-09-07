ef5f899c867ed99be322912451dfe64d86f7a7d7
Nicolas Gama <nicolas@inpher.io>
2018-09-07 13:16:35 +0200

    write dimensions in a separate file that will be available during the cloud computation

--- fhe/data_reader.cpp
+++ fhe/data_reader.cpp
@@ -10,6 +10,25 @@ int64_t read_dims_from_data(int64_t index) {
     static int64_t dims[4]; // n, k, m, lvl0_n
     static bool initialized = false;
     if (!initialized) {
+        //try to initialize the dimensions from the dataset if it is available, else set it from the dimensions.txt
+        {
+            ifstream ifs("data/covariates.csv");
+            if (!ifs) {
+                //try to read from dimensions.txt
+                ifstream dfs("dimensions.txt");
+                assert_dramatically(ifs, "dimensions.txt not found");
+                string buf;
+                dfs >> buf >> dims[0]
+                    >> buf >> dims[1]
+                    >> buf >> dims[2]
+                    >> buf >> dims[3];
+                dfs.close();
+                cout << "dimensions: n,k,m,n_lvl0: " << dims[0] << "," << dims[1]
+                     << "," << dims[2] << "," << dims[3] << endl;
+                initialized = true;
+                return dims[index];
+            }
+        }
         //read n and k from covariates.csv
         string line;
         int64_t n = -1;
@@ -61,6 +80,12 @@ int64_t read_dims_from_data(int64_t index) {
         dims[1] = k;
         dims[2] = m;
         dims[3] = n_lvl0;
+        ofstream ofs("dimensions.txt");
+        ofs << "n: " << n << endl
+            << "k: " << k << endl
+            << "m: " << m << endl
+            << "n_lvl0: " << n_lvl0 << endl;
+        ofs.close();
         cout << "dimensions: n,k,m,n_lvl0: " << n << "," << k << "," << m << "," << n_lvl0 << endl;
         initialized = true;
     }
